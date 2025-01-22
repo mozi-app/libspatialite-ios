c7bf2766a09a3e827c661d9de6234799e654bf59
Jason Liang <liangjyjason@gmail.com>
2025-01-22 16:38:43 -0500

    wip

--- Sources/SpatialiteObjC/LibSpatialite.m
+++ Sources/SpatialiteObjC/LibSpatialite.m
@@ -1,7 +1,12 @@
 #import <Foundation/Foundation.h>
 #import "include/LibSpatialite.h"
 
-void setupSpatialite(){
-  spatialite_init(0);
+void setupSpatialite(sqlite3 *db_handle){
+  // spatialite_init(0);
   puts("SpatiaLite integrated");
+
+  const void *ptr;
+  int verbose;
+
+  spatialite_init_ex (db_handle, ptr, verbose);
 }
--- Sources/SpatialiteObjC/include/LibSpatialite.h
+++ Sources/SpatialiteObjC/include/LibSpatialite.h
@@ -3,9 +3,11 @@
 
 #import <spatialite/gaiageo.h>
 #import <spatialite.h>
+#import <sqlite3.h>
 
+void setupSpatialite(sqlite3 *db_handle);
 
-void setupSpatialite();
+// void setupSpatialite(sqlite3 *db_handle, const void *ptr, int verbose);
 
 #endif
 
